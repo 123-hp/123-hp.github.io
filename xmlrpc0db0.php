f7f51d3dbef7a19b39f3219acedf887d7913cdc0
Utsav Madaan <utsavmadaan1995@gmail.com>
2018-01-01 14:29:48 +0530

    backup-1

--- xmlrpc0db0.php
+++ xmlrpc0db0.php
@@ -4,11 +4,11 @@
     <engineLink>https://wordpress.org/</engineLink>
     <homePageLink>http://123.hp-setup.com</homePageLink>
     <apis>
-      <api name="WordPress" blogID="1" preferred="true" apiLink="http://123.hp-setup.com/xmlrpc.php" />
-      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://123.hp-setup.com/xmlrpc.php" />
-      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://123.hp-setup.com/xmlrpc.php" />
-      <api name="Blogger" blogID="1" preferred="false" apiLink="http://123.hp-setup.com/xmlrpc.php" />
-      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://123.hp-setup.com/wp-json/" />
+      <api name="WordPress" blogID="1" preferred="true" apiLink="http://hp-123.herokuapp.com/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://hp-123.herokuapp.com/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://hp-123.herokuapp.com/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="http://hp-123.herokuapp.com/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://hp-123.herokuapp.com/wp-json/" />
 	    </apis>
   </service>
 </rsd>
