efc789a20172eae71edde17020a1b4f956f47f30
Utsav Madaan <utsavmadaan1995@gmail.com>
2018-01-04 17:50:31 +0530

    moved from mixed content to https

--- removed/xmlrpc0db0.php
+++ removed/xmlrpc0db0.php
@@ -1,14 +1,14 @@
-<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="http://archipelago.phrasewise.com/rsd">
+<?xml version="1.0" encoding="UTF-8"?><rsd version="1.0" xmlns="https://archipelago.phrasewise.com/rsd">
   <service>
     <engineName>WordPress</engineName>
     <engineLink>https://wordpress.org/</engineLink>
-    <homePageLink>http://hp-123.herokuapp.com</homePageLink>
+    <homePageLink>https://hp-123.herokuapp.com</homePageLink>
     <apis>
-      <api name="WordPress" blogID="1" preferred="true" apiLink="http://hp-123.herokuapp.com/xmlrpc.php" />
-      <api name="Movable Type" blogID="1" preferred="false" apiLink="http://hp-123.herokuapp.com/xmlrpc.php" />
-      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="http://hp-123.herokuapp.com/xmlrpc.php" />
-      <api name="Blogger" blogID="1" preferred="false" apiLink="http://hp-123.herokuapp.com/xmlrpc.php" />
-      	<api name="WP-API" blogID="1" preferred="false" apiLink="http://hp-123.herokuapp.com/modifications/json.html" />
+      <api name="WordPress" blogID="1" preferred="true" apiLink="https://hp-123.herokuapp.com/xmlrpc.php" />
+      <api name="Movable Type" blogID="1" preferred="false" apiLink="https://hp-123.herokuapp.com/xmlrpc.php" />
+      <api name="MetaWeblog" blogID="1" preferred="false" apiLink="https://hp-123.herokuapp.com/xmlrpc.php" />
+      <api name="Blogger" blogID="1" preferred="false" apiLink="https://hp-123.herokuapp.com/xmlrpc.php" />
+      	<api name="WP-API" blogID="1" preferred="false" apiLink="https://hp-123.herokuapp.com/modifications/json.html" />
 	    </apis>
   </service>
 </rsd>
